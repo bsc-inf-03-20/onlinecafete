359cd97d72fce887b5982b61bbc0205535d14796
bsc-inf-03-20 <chirwathoko26@gmail.com>
2026-08-19 14:49:53 +0200

    image for marketing

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -4,15 +4,21 @@ import {
   IsOptional,
   IsString,
   Length,
+  ValidateNested,
 } from 'class-validator';
+import { Type } from 'class-transformer';
 import { AddressDto } from '../../common/dto/address.dto';
 import { CreateOrderItemDto } from './create-order-item.dto';
 
 export class CreateOrderDto {
   @IsArray()
   @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => CreateOrderItemDto)
   items: CreateOrderItemDto[];
 
+  @ValidateNested()
+  @Type(() => AddressDto)
   deliveryAddress: AddressDto;
 
   @IsOptional()
